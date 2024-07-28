89e8b8a062e7e0b98ac42e7d9d2c2c5a036af523
ilyusha-durachok <moortoth@yandex.ru>
2024-07-28 18:40:24 +0400

    compiled finally

--- rust-blink/src/main.rs
+++ rust-blink/src/main.rs
@@ -1,31 +1,43 @@
-#![no_std]
 #![no_main]
-// Halt when the program panics.
-extern crate panic_halt;
-// Includes.
-use cortex_m::peripheral::syst::SystClkSource;
+#![no_std]
+
 use cortex_m_rt::entry;
-use stm32f103;
+use panic_halt as _;
+
+use stm32f103::Peripherals;
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m::peripheral::Peripherals as CortexPeripherals;
 
 #[entry]
 fn main() -> ! {
-    let p = stm32f103::Peripherals::take().unwrap();
-//   let rcc = p.RCC;
-//   rcc.iopenr.write( |w| w.iopben().set_bit() );
-//   let gpioc = p.GPIOC;
-//   unsafe { gpioc.moder.write( |w| w.mode13().bits( 0b01 ) ); }
-//   gpioc.otyper.write( |w| w.ot13().clear_bit() );
-//   // Restart the SysTick counter.
-//   syst.clear_current();
-//   // Main loop.
-//   loop {
-//     // Toggle the LED every SysTick tick.
-//     while !syst.has_wrapped() {};
-//     gpioc.odr.write( |w| w.od13().set_bit() );
-//     while !syst.has_wrapped() {};
-//     gpioc.odr.write( |w| w.od13().clear_bit() );
-//   }
+    // Получаем доступ к периферийным устройствам
+    let dp = Peripherals::take().unwrap();
+    let cp = CortexPeripherals::take().unwrap();
+
+    // Включаем тактирование GPIOC
+    let rcc = dp.rcc;
+    rcc.apb2enr().modify(|_, w| w.iopcen().set_bit());
+
+    // Конфигурируем PC13 как выход
+    let gpioc = dp.gpioc;
+    unsafe {gpioc.crh().modify(|_, w| w.mode13().bits( 0b01 )); }
+
+    // Получаем доступ к системному таймеру
+    let mut syst = cp.SYST;
+
+    // Устанавливаем источник тактирования для системного таймера
+    syst.set_clock_source(SystClkSource::Core);
+
+    // Задаем период таймера (1 секунда)
+    syst.set_reload(8_000_000); // 8 MHz для простоты, настраиваем на 1 секунду
+    syst.enable_counter();
+    syst.enable_interrupt();
 
     loop {
+        // Ждем срабатывания таймера
+        while !syst.has_wrapped() {}
+
+        // Переключаем состояние светодиода
+        gpioc.odr().modify(|r, w| w.odr13().bit(!r.odr13().bit()));
     }
 }
